4d5c8db756fa4ee20d0fdf5b578df6db7a911533
shrelhan <shrelhan.relhan@gmail.com>
2017-10-01 17:57:22 +0530

    Starting new project

---
+++ main.py
@@ -0,0 +1,12 @@
+print "Let\'s gets started"
+
+# Below we are saving the values of spy like name and printing a statement after getting the value
+spy_name = raw_input("What is your name ?")
+spy_salutation = raw_input("What should we call you (Mr. or Mrs.) ?")
+
+# Below method is called re-assigning the variable value to new one
+spy_name = spy_salutation + " " + spy_name
+
+# print is used to print any statement carrying inputs
+print "Welcome " + spy_name + ". Glad to have you back with us."
+
